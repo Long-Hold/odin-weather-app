5776587a737fff8a2e6fffc3858867c6fed4ea60
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-13 14:56:53 -0500

    refactor: Add early error check for empty paramter

--- src/modules/weatherService.js
+++ src/modules/weatherService.js
@@ -6,9 +6,14 @@ const API_LINK = 'https://weather.visualcrossing.com/VisualCrossingWebServices/r
  * 
  * @param {string} location - The location to fetch weather data for.
  * @returns {Promise<Object>} Weather data object from the API.
+ * @throws {Error} When location parameter is empty.
  * @throws {Error} When the API request fails.
  */
 export async function getWeatherData(location) {
+    if (!location || location.trim()) {
+        throw new Error('Location cannot be blank.');
+    }
+
     const request = API_LINK + location + `?key=${API_KEY}`;
 
     const result = await fetch(request);
