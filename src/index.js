04c0c4c75b4e6a18c52142c204bd39b1591e1914
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-13 14:53:39 -0500

    refactor: Catch errors thrown by the getWeatherData API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { getWeatherData } from "./modules/weatherService";
 
 const button = document.querySelector('button');
-button.addEventListener('click', () => console.log(getWeatherData('Toronto')));
+button.addEventListener('click', async () => {
+    try {
+        const weatherData = await getWeatherData('Toronto');
+        console.log(weatherData);
+    } catch(error) {
+        console.error(`An error was caught while retrieving weather data. ${error}`);
+    }
+});
