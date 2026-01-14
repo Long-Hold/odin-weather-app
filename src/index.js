1cf60256251bd1ef7b0e3a4c954ab12201e581aa
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-14 18:03:14 -0500

    refactor: Run prettier for standardized format

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import { parseWeatherJson } from "./modules/weatherClass";
 import { getWeatherData } from "./modules/weatherService";
 
-const form = document.querySelector('form');
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const location = formData.get('location').trim().toLowerCase();
+const form = document.querySelector("form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const location = formData.get("location").trim().toLowerCase();
 
-    try {
-        const weatherData = await getWeatherData(location);
-        const weatherObject = parseWeatherJson(weatherData);
-        console.log(weatherObject);
-    } catch (error) {
-        console.error(`An error was caught while retrieving weather data. ${error}`);
-    } finally {
-        form.reset();
-    }
+  try {
+    const weatherData = await getWeatherData(location);
+    const weatherObject = parseWeatherJson(weatherData);
+    console.log(weatherObject);
+  } catch (error) {
+    console.error(
+      `An error was caught while retrieving weather data. ${error}`,
+    );
+  } finally {
+    form.reset();
+  }
 });
--- src/modules/weatherService.js
+++ src/modules/weatherService.js
@@ -1,28 +1,36 @@
-const API_KEY = 'SYJEG9VMFX7C945UL7FBJ8JH7';
-const API_LINK = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-const API_GROUPS = '&elements=add:aqius,remove:datetimeEpoch,remove:dew,remove:feelslikemax,remove:feelslikemin,remove:solarenergy,remove:solarradiation,remove:stations';
-const UNIT_GROUP = '&unitGroup=metric';
+const API_KEY = "SYJEG9VMFX7C945UL7FBJ8JH7";
+const API_LINK =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const API_GROUPS =
+  "&elements=add:aqius,remove:datetimeEpoch,remove:dew,remove:feelslikemax,remove:feelslikemin,remove:solarenergy,remove:solarradiation,remove:stations";
+const UNIT_GROUP = "&unitGroup=metric";
 /**
  * Fetches weather data from the VisualCrossing API.
  * This function will retrieve the next 7 days worth of forecast data.
- * 
+ *
  * @param {string} location - The location to fetch weather data for.
  * @returns {Promise<Object>} Weather data object from the API.
  * @throws {Error} When location parameter is empty.
  * @throws {Error} When the API request fails.
  */
 export async function getWeatherData(location) {
-    if (!location || !location.trim()) {
-        throw new Error('Location cannot be blank.');
-    }
+  if (!location || !location.trim()) {
+    throw new Error("Location cannot be blank.");
+  }
 
-    const request = API_LINK + location + '/next7days/' + `?key=${API_KEY}` + UNIT_GROUP + API_GROUPS;
+  const request =
+    API_LINK +
+    location +
+    "/next7days/" +
+    `?key=${API_KEY}` +
+    UNIT_GROUP +
+    API_GROUPS;
 
-    const result = await fetch(request);
-    if (!result.ok) {
-        throw new Error(result.status);
-    }
+  const result = await fetch(request);
+  if (!result.ok) {
+    throw new Error(result.status);
+  }
 
-    const data = await result.json();
-    return data;
+  const data = await result.json();
+  return data;
 }
