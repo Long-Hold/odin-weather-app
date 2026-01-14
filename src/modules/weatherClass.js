46c79fbf83311e587007bc640444b7077f51a456
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-14 16:38:00 -0500

    feat: Add functions to extract current and forecasted data into objects

--- src/modules/weatherClass.js
+++ src/modules/weatherClass.js
@@ -2,7 +2,51 @@ export function parseWeatherJson(weatherData) {
     return {
         address: weatherData.address,
         description: weatherData.description,
-        cloudCover: weatherData.currentConditions.cloudcover,
-        feelsLike: weatherData.currentConditions.feelslike,
+        current: extractCurrentConditions(weatherData.currentConditions),
+        forecasted: extractForecastedConditions(weatherData.days),
     }
+}
+
+function extractCurrentConditions(current) {
+    return {
+        airQualityIndex: current.aqius,
+        cloudCover: current.cloudcover,
+        conditions: current.conditions,
+        feelsLike: current.feelslike,
+        humidity: current.humidity,
+        precip: current.precip,
+        precipProb: current.precipprob,
+        precipType: current.preciptype,
+        presure: current.pressure,
+        snow: current.snow,
+        snowDepth: current.snowdepth,
+        sunrise: current.sunrise,
+        sunset: current.sunset,
+        temp: current.temp,
+        visibility: current.visibility,
+        windDirection: current.winddir,
+        windGust: current.windgust,
+        windSpeed: current.windspeed,
+    }
+}
+
+function extractForecastedConditions(days) {
+    return days.map((day) => ({
+        date: day.datetime,
+        temp: day.temp,
+        tempMax: day.tempmax,
+        tempMin: day.tempmin,
+        feelsLike: day.feelslike,
+        humidity: day.humidity,
+        conditions: day.conditionns,
+        description: day.description,
+        precip: day.precip,
+        precipProb: day.precipprob,
+        precipType: day.preciptype[day.preciptype.length - 1],
+        snow: day.snow,
+        snowDepth: day.snowdepth,
+        windDirection: day.winddir,
+        windGust: day.windgust,
+        windSpeed: day.windspeed
+    }))
 }
