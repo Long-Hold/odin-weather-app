09a7b1454d583ab140c840b940e4ee3da4da1893
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-21 10:32:27 -0500

    refactor: Query select elements with a dataset to property name system

--- src/modules/weatherMarkupController.js
+++ src/modules/weatherMarkupController.js
@@ -4,53 +4,24 @@ const forecastCardTemplate = document.getElementById('forecast-card-template');
 /**
  * Updates the DOM element that represents current weather data with information
  * from the current weather object.
- * @param {Object} weatherData 
+ * @param {Object} weatherData - An object containing weather data from the Visual Crossing API.
  */
 export function displayCurrentWeatherData(weatherData) {
+    /**These values are not in the nested 'current' object,
+     * so they are queried and set manually
+     */
     const address = currentWeatherCard.querySelector('.address');
     const description = currentWeatherCard.querySelector('.description');
-    const currentTime = currentWeatherCard.querySelector('.current-time');
-    const icon = currentWeatherCard.querySelector('img');
-    
-    const temperature = currentWeatherCard.querySelector('.temperature');
-    const feelsLike = currentWeatherCard.querySelector('.feels-like');
-    const conditions = currentWeatherCard.querySelector('.conditions');
-    const humidity = currentWeatherCard.querySelector('.humidity');
-
-    const precip = currentWeatherCard.querySelector('.precip');
-    const precipProb = currentWeatherCard.querySelector('.precip-prob');
-    const precipType = currentWeatherCard.querySelector('.precip-type');
-    const snow = currentWeatherCard.querySelector('.snow');
-
-    const sunrise = currentWeatherCard.querySelector('.sunrise');
-    const sunset = currentWeatherCard.querySelector('.sunset');
-
-    const windSpeed = currentWeatherCard.querySelector('.wind-speed');
-    const windDirection = currentWeatherCard.querySelector('.wind-direction');
-    const windGust = currentWeatherCard.querySelector('.wind-gust');
-
     address.textContent = weatherData.address;
-    description.textContent = weatherData.description;
-    // TODO: Fetch the corresponding icon here
+    description.textContent = weatherData.description
+    //TODO: Set Icon
 
     const current = weatherData.current;
-    currentTime.textContent = current.datetime;
-    temperature.textContent = current.temp;
-    feelsLike.textContent = current.feelsLike;
-    conditions.textContent = current.conditions;
-    humidity.textContent = current.humidity;
-
-    precip.textContent = current.precip;
-    precipProb.textContent = current.precpProb;
-    precipType.textContent = current.precipType;
-    snow.textContent = current.snow;
-
-    sunrise.textContent = current.sunrise;
-    sunset.textContent = current.sunset;
-
-    windSpeed.textContent = current.windSpeed;
-    windDirection.textContent = current.windDirection;
-    windGust.textContent = current.windGust;
+    for (const [key, value] of Object.entries(current)) {
+        const element = currentWeatherCard.querySelector(`[data-forecast="${key}"]`);
+        if (element) element.textContent = value;
+        else console.error(`Element for current weather ${key} not found.`);
+    }
 }
 
 /**
