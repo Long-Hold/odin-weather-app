fcec173c41d54dc338ae928c03aff09a3a0d17c2
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-21 14:04:28 -0500

    refactor: Add try catch blocks for failed image retrieval

--- src/modules/dom/weatherIconManager.js
+++ src/modules/dom/weatherIconManager.js
@@ -1,5 +1,17 @@
 const icons = require.context('../../assets/icons', false, /\.svg$/);
 
+/**
+ * Searches for an icon that matches the passed parameter.
+ * If the icon cannot be found, a default placeholder is returned instead.
+ * 
+ * @param {string} iconName - The string name of the icon to search for.
+ * @returns {string}
+ */
 export function getWeatherIcon(iconName) {
-    return icons(`./${iconName}.svg`);
+    try {
+        return icons(`./${iconName}.svg`);
+    } catch (error) {
+        console.error(`Image not found: ${iconName}`);
+        return icons('./image-not-found.svg');
+    }
 }
