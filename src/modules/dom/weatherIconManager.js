e52ac34032bd687572c456b3bf8391c79abd45f6
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-26 01:00:31 -0500

    feat: Add new function to retrieve thumbnails

--- src/modules/dom/weatherIconManager.js
+++ src/modules/dom/weatherIconManager.js
@@ -1,4 +1,5 @@
-const icons = require.context('../../assets/icons', false, /\.svg$/);
+const icons = require.context('../../assets/icons/weatherIcons', false, /\.svg$/);
+const thumbnails = require.context('../../assets/icons/thumbnails', false, /\.png$/);
 
 /**
  * Searches for an icon that matches the passed parameter.
@@ -13,6 +14,23 @@ export function getWeatherIcon(iconName) {
     } catch (error) {
         console.error(error);
         console.error(`Image not found: ${iconName}`);
+        return icons('./image-not-found.svg');
+    }
+}
+
+/**
+ * Searches for a thumbnail that matches the passed paramter.
+ * If the thumbnail cannot be found, a default placeholder is returned instead.
+ * 
+ * @param {string} thumbnailName - The string name of the thumbnail to search for.
+ * @returns {string} A string of the image filepath
+ */
+export function getDataThumbnail(thumbnailName) {
+    try {
+        return thumbnails(`./${thumbnailName}.png`);
+    } catch(error) {
+        console.error(error);
+        console.error(`Image not found: ${thumbnailName}`);
         return icons('./image-not-found.svg');
     }
 }
