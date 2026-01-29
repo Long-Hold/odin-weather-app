065fe16f6b497161aaa30d201a91a8c9dc2ec787
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-01-28 19:17:27 -0500

    feat: Create functions to add an remove loading icon

---
+++ src/modules/dom/loadingIconController.js
@@ -0,0 +1,10 @@
+export function renderLoadingIcon() {
+    const div = document.createElement('div');
+    div.classList.add('loader');
+    document.body.appendChild(div);
+    return div;
+}
+
+export function removeLoadingIcon(element) {
+    element?.remove();
+}
